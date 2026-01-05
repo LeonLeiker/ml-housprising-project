f427b3e62e41e6b86e18747c4ae3d2e35393538a
Leon Leiker <leonleiker13@gmail.com>
2026-01-05 14:18:59 +0100

    Modify Gradient Descent for n features

--- main.py
+++ main.py
@@ -10,18 +10,17 @@ def main():
     m = len(y) # number of training examples
     n = len(x_standardized[0]) # number of features
     vector_theta = [0.0 for _ in range(n)]
-    alpha = 0.001
-    training_epochs = 3000
+    alpha = 0.01
+    training_epochs = 2000
 
     for i in range(training_epochs):
-        y_hat = calculate_predictions_of_hypothesis(x_standardized, theta_0, theta_1)
+        y_hat, vector_theta = calculate_predictions_of_hypothesis(x_standardized, vector_theta)
         J = calculate_value_of_loss_function(y_hat, y, m)
         J_sqrt.append((i, statistics.sqrt(2 * J)))
         if i % 10 == 0:
             print(f"Epoch {i}: Loss Function Value = {J}")
-            print(f"Theta 0: {theta_0}, Theta 1: {theta_1}")
-        d_theta_0, d_theta_1 = derivative_cost_function(y_hat, y, x_standardized, m)
-        theta_0, theta_1 = update_parameters(theta_0, theta_1, d_theta_0, d_theta_1, alpha)
+        gradient = derivative_cost_function(x_standardized, y, y_hat, m, n)
+        vector_theta = update_parameters(vector_theta, gradient, alpha)
 
     plot_and_save_loss_function(J_sqrt)
     plot_and_save_training_data(x, y, y_hat)
--- models.py
+++ models.py
@@ -1,39 +1,40 @@
 
 
-def calculate_predictions_of_hypothesis(x, theta_0, theta_1):
+def calculate_predictions_of_hypothesis(x, vector_theta):
     y_hat = []
     for x_i in x:
-        h_of_x = theta_1 * x_i + theta_0
+        interim_results = []
+        for feature_i, theta_i in zip(x_i, vector_theta):
+            interim_results.append(feature_i * theta_i)
+        h_of_x = sum(interim_results)
         y_hat.append(h_of_x)
 
-    return y_hat
+    return y_hat, vector_theta
 
 
 def calculate_value_of_loss_function(y_hat, y, m):
     J = 0
-    for y_1, y_hat_1 in zip(y, y_hat):
-        J += (y_hat_1 - y_1) ** 2
+    for y_i, y_hat_i in zip(y, y_hat):
+        J += (y_hat_i - y_i) ** 2
     J = J / (2 * m)
 
     return J
 
 
-def derivative_cost_function(y_hat, y, x, m):
-    d_theta_0 = 0
-    d_theta_1 = 0
+def derivative_cost_function(x_standardized, y, y_hat, m, n):
+    gradient = [0.0 for _ in range(n)]
+    for i in range(m):
+        error = y_hat[i] - y[i]
+        for j in range(n):
+            gradient[j] += error * x_standardized[i][j]
 
-    for y_hat_1, y_1, x_1 in zip(y_hat, y, x):
-        d_theta_0 += (y_hat_1 - y_1)
-        d_theta_1 += (y_hat_1 - y_1) * x_1
+    gradient = [(g_i / m) for g_i in gradient]
 
-    d_theta_0 = d_theta_0 / m
-    d_theta_1 = d_theta_1 / m
-
-    return d_theta_0, d_theta_1
+    return gradient
 
 
-def update_parameters(theta_0, theta_1, d_theta_0, d_theta_1, alpha):
-    theta_0 = theta_0 - alpha * d_theta_0
-    theta_1 = theta_1 - alpha * d_theta_1
+def update_parameters(vector_theta, gradient, alpha):
+    for i in range(len(vector_theta)):
+        vector_theta[i] = vector_theta[i] - alpha * gradient[i]
 
-    return theta_0, theta_1
+    return vector_theta
